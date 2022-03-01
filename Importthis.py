9067b213052434a79d9f06dc677b1a24f335d8f3
DariaMajc <daria.majchrowska@wp.pl>
2022-03-01 22:41:58 +0100

    update

--- Importthis.py
+++ Importthis.py
@@ -1,4 +1,4 @@
-Zen_of_Phyton = 'Beautiful is better than ugly.
+Zen_of_Python = '''Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
 Complex is better than complicated.
@@ -16,4 +16,26 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!'
+Namespaces are one honking great idea -- let's do more of those!'''
+
+#Policz liczbę wystąpień słowa better.
+print(Zen_of_Python.count('better'))
+
+print('----------------------------')
+#Usuń z tekstu symbol gwiazdki
+print(Zen_of_Python.replace("*", " "))
+
+print('----------------------------')
+#Zamień jedno wystąpienie explain na understand
+replace = 'explain'
+replace2 = 'understand'
+print(Zen_of_Python.find(replace))
+print(Zen_of_Python[:667], replace2, Zen_of_Python[677:])
+
+print('----------------------------')
+#Usuń spacje i połącz wszystkie słowa myślnikiem
+Zen_of_Python2 = Zen_of_Python.replace(" ", "")
+print("-".join(Zen_of_Python2))
+
+print('----------------------------')
+#Podziel tekst na osobne zdania za pomocą kropki
---
+++ twovariables.py
@@ -0,0 +1,6 @@
+#Stwórz dwie dowolne zmienne przechowujące wartość liczbową i tekstową.
+#Użyj funkcji format(), by wyświetlić zdanie zawierające te wartości.
+
+soon = 'Ignacek'
+soon_age = 2
+print('{} ma {} lat.'.format(soon, soon_age))
